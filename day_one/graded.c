5f0a98cf086982e49c5ad044550a10422f9d447e
cameronotoole44 <cameronotoole404@gmail.com>
2025-04-01 13:36:11 -0400

    initial commit

---
+++ day_one/graded.c
@@ -0,0 +1,17 @@
+//  fix the mistake //
+
+//  DR.P //
+// #include<stdio.h>
+// int main(void)
+// {   
+//     printf(‘ hello world\n’);
+//     return 0;
+// }   
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello world\n");
+    return 0;
+}
---
+++ day_one/poem.c
@@ -0,0 +1,35 @@
+//  use the printf() statement to print a poem //
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Take this kiss upon the brow!\n"
+           "And, in parting from you now,\n"
+           "Thus much let me avow —\n"
+           "You are not wrong, who deem\n"
+           "That my days have been a dream;\n"
+           "Yet if hope has flown away\n"
+           "In a night, or in a day,\n"
+           "In a vision, or in none,\n"
+           "Is it therefore the less gone?\n"
+           "All that we see or seem\n"
+           "Is but a dream within a dream.\n"
+           "\n"
+           "I stand amid the roar\n"
+           "Of a surf-tormented shore,\n"
+           "And I hold within my hand\n"
+           "Grains of the golden sand —\n"
+           "How few! yet how they creep\n"
+           "Through my fingers to the deep,\n"
+           "While I weep — while I weep!\n"
+           "O God! Can I not grasp\n"
+           "Them with a tighter clasp?\n"
+           "O God! can I not save\n"
+           "One from the pitiless wave?\n"
+           "Is all that we see or seem\n"
+           "But a dream within a dream?\n"
+           "\"A Dream Within a Dream\"\n"
+           "- Edgar Allan Poe");
+    return 0;
+}
