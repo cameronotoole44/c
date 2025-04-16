05b0d7fa4380397fc53af50feaf9a8414988e50e
cameronotoole44 <cameronotoole404@gmail.com>
2025-04-15 21:57:33 -0400

    comment added

--- day_two/main.c
+++ day_two/main.c
@@ -23,3 +23,20 @@ int main(void)
            a, b, c, d);
     return 0;                                  
 }
+
+/* 
+
+> gcc main.c -o main.exe
+
+> ./main.exe
+
+> $ ./main.exe
+a = 5, b = 7, c =-2, d =0
+a = 5, b = 7, c =2, d =0
+a = 5, b = 7, c =0, d =1
+a = 5, b = 7, c =5, d =2
+a = 5, b = 7, c =-12, d =-12
+a = 6, b = 8, c =13, d =-7
+
+
+*/
