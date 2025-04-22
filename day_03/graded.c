ffdefa4071d5ef6e84f82e5c5d87c72909178239
cameronotoole44 <cameronotoole404@gmail.com>
2025-04-22 09:48:48 -0400

    fixed version, and compiled

--- day_03/graded.c
+++ day_03/graded.c
@@ -1,5 +1,6 @@
 /* The following code is supposed to produce the volume of a sphere as a double but is using ints. Fix this issue. */
 
+/*
 #include<stdio.h>
 #define PI 3.14159
 int main(void)
@@ -9,4 +10,21 @@ int main(void)
  scanf(“%d”, &radius);
 printf(“volume is : %d \n\n“, 4 *radius*radius*radius/3 );
 return 0;
+}
+*/
+
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main(void)
+{ 
+    double radius;
+    printf("Enter radius: ");
+    scanf("%lf", &radius);
+
+    double volume = (4.0 / 3.0) * PI * radius * radius * radius;
+    printf("Volume is: %lf\n\n", volume);
+
+    return 0;
 }
