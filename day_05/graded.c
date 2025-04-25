be2fa0fd797ad0c7fb16d27339f00311a97eeec7
cameronotoole44 <cameronotoole404@gmail.com>
2025-04-24 21:47:06 -0400

    use fabs instead of abs

--- day_05/graded.c
+++ day_05/graded.c
@@ -7,8 +7,8 @@ int main(void)
     int i;
     for(i = 0; i < 30; i++)
     {
-        interval = i/10.0;
-        printf("sin( %lf ) = %lf \t" , interval, abs(sin(interval)));
+        interval = i / 10.0;
+        printf("sin( %lf ) = %lf\t", interval, fabs(sin(interval)));
     }
     printf("\n+++++++\n");
     return 0;
