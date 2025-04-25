c84b30039d2478e1ad13c39e6f669c560e85890b
cameronotoole44 <cameronotoole404@gmail.com>
2025-04-24 21:26:33 -0400

    responses

--- day_05/switch.c
+++ day_05/switch.c
@@ -11,3 +11,49 @@ switch(i) {
         printf("default case \n");
 };
 
+// if i = 1
+
+// switch(i) {
+//     case 1: 
+//         printf("case 1 \n");  // line runs
+//         break;                // exits the switch, so nothing else runs
+//     case 2: 
+//         printf("case 2 \n");
+//     case 3: 
+//         printf("case 3 \n");
+//         break;
+//     default: 
+//         printf("default case \n");
+// };
+
+
+// if i = 15
+
+// switch(i) {
+//     case 1: 
+//         printf("case 1 \n"); 
+//         break;
+//     case 2: 
+//         printf("case 2 \n");
+//     case 3: 
+//         printf("case 3 \n");
+//         break;
+//     default: 
+//         printf("default case \n");
+// };
+
+// if i = 2 
+
+// switch(i) {
+//     case 1: 
+//         printf("case 1 \n"); 
+//         break;
+//     case 2: 
+//         printf("case 2 \n");
+//     case 3: 
+//         printf("case 3 \n");
+//         break;
+//     default: 
+//         printf("default case \n");
+// };
+
